be8b4d47ee74399b008daea2eb02de3668309064
Андрей <Winterles@yandex.ru>
2025-10-18 01:07:49 +0300

    Тест

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './Router/routes';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './Store/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={routes} />
+    </Provider>
+  </StrictMode>
+);
