51db6ddc4440e5311569bb1e50a299d513c2053d
Андрей <Winterles@yandex.ru>
2025-10-18 01:40:07 +0300

    #

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './Router/routes';
-import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './Store/store';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
