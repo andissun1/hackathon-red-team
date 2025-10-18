6247bfef7eabd55d63427610b248e3925c1daf86
Maria Bezzubova <anmari1112@gmail.com>
2025-10-18 18:30:04 +0300

    добавила пропс для кнопки

--- src/Shared/Button/Button.jsx
+++ src/Shared/Button/Button.jsx
@@ -1,9 +1,16 @@
 import style from "./button.module.css";
-export const Button = ({ children, rounded = "rounded", onClick }) => {
+export const Button = ({
+  children,
+  rounded = "rounded",
+  cancel = false,
+  onClick,
+}) => {
   return (
     <button
       onClick={onClick}
-      className={`${style.button} ${style[rounded] ? style[rounded] : ""}`}
+      className={`${style.button} ${style[rounded] ? style[rounded] : ""} ${
+        cancel ? style.cancel : ""
+      }`}
     >
       {children}
     </button>
