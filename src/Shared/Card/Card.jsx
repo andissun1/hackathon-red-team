2fadd2dec1fc9c620d8f9b9701bd55c1ac2665b0
Андрей <Winterles@yandex.ru>
2025-10-18 11:50:42 +0300

    Код с логикой и стилями

--- src/Shared/Card/Card.jsx
+++ src/Shared/Card/Card.jsx
@@ -1,40 +1,47 @@
-import { Badge } from "../Badge/Badge";
-import { Button } from "../Button/Button";
-import HeartIcon from "../HeartIcon/HeartIcon";
-import { Text } from "../Text/Text";
-import style from "./card.module.css";
-import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { Badge } from '../Badge/Badge';
+import { Button } from '../Button/Button';
+import HeartIcon from '../HeartIcon/HeartIcon';
+import { Text } from '../Text/Text';
+import style from './card.module.css';
+import { useNavigate } from 'react-router-dom';
+import { appActions } from '../../Store/appReducer';
 
 export const Card = ({
   id,
   name,
   surname,
   age,
-  photo = "./../../../public/default-photo.jpg",
+  imgURL = './../../../public/default-photo.jpg',
   about,
   favourite = false,
   badge = null,
 }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const openProfile = () => {
     navigate(`/profile/${id}`);
   };
   const onToggleFavourite = () => {
-    console.log("favourite toggle");
+    favourite
+      ? dispatch(appActions.removeFromFavorites(id))
+      : dispatch(appActions.addToFavorites(id));
   };
+
+  console.log(imgURL);
+
   return (
     <div className={style.card}>
-      <img className={style.img} src={photo} />
+      <img className={style.img} src={imgURL} />
       <div className={style.name}>
-        {surname} {name}, {age}{" "}
-        {badge && <Badge color={badge.color} text={badge.text} />}
+        {surname} {name}, {age} {badge && <Badge color={badge.color} text={badge.text} />}
       </div>
       <div className={style.subtitle}>Немного обо мне:</div>
       <Text limited={true}>{about}</Text>
       <div className={style.buttonContainer}>
         <Button onClick={openProfile}>ОТКРЫТЬ</Button>
         <Button onClick={onToggleFavourite}>
-          <HeartIcon variant={favourite ? "filled" : "outline"} />
+          <HeartIcon variant={favourite ? 'filled' : 'outline'} />
         </Button>
       </div>
     </div>
