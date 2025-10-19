b148c4c5c105ef508f6bcc0c660951ea7e8e1961
Maria Bezzubova <anmari1112@gmail.com>
2025-10-19 11:59:59 +0300

    Добавила светлую\темную темы

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,28 @@
+import { Link } from "react-router-dom";
 import style from "./footer.module.css";
 
-export const Footer = (props) => {
+export const Footer = () => {
+  const teamMembers = [
+    { id: "02", name: "Андрей" },
+    { id: "03", name: "Илья" },
+    { id: "01", name: "Мария" },
+  ];
+
   return (
     <footer className={style.footer}>
       <div className={style.footerContent}>
         <div>Сайт создан на хакатоне 2025</div>
-        <div>Над сайтом работали: Андрей, Илья, Мария</div>
+        <div>
+          Над сайтом работали:{" "}
+          {teamMembers.map((member, index) => (
+            <span key={member.id}>
+              <Link className={style.link} to={`/profile/${member.id}`}>
+                {member.name}
+              </Link>
+              {index < teamMembers.length - 1 ? ", " : ""}
+            </span>
+          ))}
+        </div>
       </div>
     </footer>
   );
